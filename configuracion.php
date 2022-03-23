9e39041ab59f7bcb8b54ed1373fb308a5f7243b3
Elialcala7 <101678734+Elialcala7@users.noreply.github.com>
2022-03-23 08:41:38 -0300

    Archivos
    Respaldo de la base de datos manual y archivos que deben ir dentro del proyecto

---
+++ configuracion.php
@@ -0,0 +1,10 @@
+<?php
+	$cadenaConexionGlobal="conexion/conexion1.php";
+	$plantilla="sistema";
+	$nombreSistema="Sistema";
+	$nombreArchivo="sistema";
+	$mostrarErroresPHP="no"; 
+	$mostrarConsultasSQL="no";
+	$moduloDesarrollador="no"; 
+	$RutaBase="Sistema_Cobranza_CorpoCapital";
+?>
